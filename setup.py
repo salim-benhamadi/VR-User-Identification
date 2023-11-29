b30f6bdcdc21e952c44b5be2b44350650a5790a5
salim-benhamadi-prorob <salim.benhamadi@prorob.it>
2023-11-29 14:58:51 +0100

    create the main repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Comparative Study of Virtual Motion and Network Traffic Data for Predicting User Age, Gender, and Private Attributes',
+    author='Salim Benhamadi',
+    license='',
+)
